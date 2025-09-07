28de607fb959bca5d29a0bf5435cf81517567594
Micah <166214972+back1drop@users.noreply.github.com>
2025-09-07 09:51:09 +0300

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,14 @@
 from .models import Book, Author, Library
 
-# Query all books by a specific author
-def books_by_author(author):
+
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
     return Book.objects.filter(author=author)
 
-# List all books in a specific library
 def books_in_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.books.all()
 
-# Retrieve the librarian for a library
 def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian  # because of OneToOneField
+    return library.librarian  
