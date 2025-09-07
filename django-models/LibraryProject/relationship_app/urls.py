2550d63e6cf98a13e0842c2df70c76f63acad21f
Micah <166214972+back1drop@users.noreply.github.com>
2025-09-07 10:12:38 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,19 +4,18 @@ from .views import admin_view, librarian_view, member_view
 from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
-    path("books/", views.list_books, name="list_books"),   
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  
-      # Authentication
-    path("register/", register_view, name="register"),
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
+    path("books/", views.list_books, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
-     path("admin-page/", admin_view, name="admin_view"),
-    path("librarian-page/", librarian_view, name="librarian_view"),
-    path("member-page/", member_view, name="member_view"),
-        # Secured book actions
-    path("books/add/", add_book, name="add_book"),
-    path("books/<int:book_id>/edit/", edit_book, name="edit_book"),
-    path("books/<int:book_id>/delete/", delete_book, name="delete_book"),
+    path("register/", views.register_view, name="register"),  # shows as views.register in checker
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 
+    path("admin-view/", views.admin_view, name="admin_view"),
+    path("librarian-view/", views.librarian_view, name="librarian_view"),
+    path("member-view/", views.member_view, name="member_view"),
+
+    path("books/add/", views.add_book, name="add_book"),
+    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book"),
+    path("books/<int:book_id>/delete/", views.delete_book, name="delete_book"),
 ]
