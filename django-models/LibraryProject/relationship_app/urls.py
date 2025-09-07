7b6954ede6e4fd084553eadbe3d943694a8fa20a
Micah <166214972+back1drop@users.noreply.github.com>
2025-09-07 10:25:29 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("books/", views.list_books, name="list_books"),
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
-    path("register/", views.register_view, name="register"),  # shows as views.register in checker
+    path("register/", views.register_view, name="register"),
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 
@@ -15,7 +15,7 @@ urlpatterns = [
     path("librarian-view/", views.librarian_view, name="librarian_view"),
     path("member-view/", views.member_view, name="member_view"),
 
-    path("books/add/", views.add_book, name="add_book"),
-    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book"),
-    path("books/<int:book_id>/delete/", views.delete_book, name="delete_book"),
+    path("add_book/", views.add_book, name="add_book"),  
+    path("edit_book/<int:book_id>/", views.edit_book, name="edit_book"), 
+    path("delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
 ]
