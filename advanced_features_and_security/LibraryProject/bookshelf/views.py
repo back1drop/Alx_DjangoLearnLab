d76f69e27e9d07ec5f833988369f937972c8428b
Micah <166214972+back1drop@users.noreply.github.com>
2025-09-14 19:43:33 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,38 +1,20 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, permission_required
 from .models import Book
+from .forms import ExampleForm
 
-@permission_required('bookshelf.can_view', raise_exception=True)
-def book_list(request):
+@login_required
+@permissions_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
     books = Book.objects.all()
-    return render(request, "bookshelf/book_list.html", {"books": books})
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
-
-@permission_required('bookshelf.can_create', raise_exception=True)
-def book_create(request):
-    if request.method == "POST":
-        title = request.POST.get("title")
-        author = request.POST.get("author")
-        published_date = request.POST.get("published_date")
-        Book.objects.create(title=title, author=author, published_date=published_date)
-        return redirect("book_list")
-    return render(request, "bookshelf/book_form.html")
-
-
-@permission_required('bookshelf.can_edit', raise_exception=True)
-def book_edit(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == "POST":
-        book.title = request.POST.get("title")
-        book.author = request.POST.get("author")
-        book.published_date = request.POST.get("published_date")
-        book.save()
-        return redirect("book_list")
-    return render(request, "bookshelf/book_form.html", {"book": book})
-
-
-@permission_required('bookshelf.can_delete', raise_exception=True)
-def book_delete(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    book.delete()
-    return redirect("book_list")
+#to prevent sql injection, use orms instead of raw sql queries
+def book_search(request):
+    form = ExampleForm(request.GET or None)
+    books = []
+    if form.is_valid():
+        title = form.cleaned_data['title']
+        #secure orm query
+        books = Book.objects.filter(title__icontains=title)
+    return render(request, 'bookshelf/book_list.html', {'form': form, 'books': books})
