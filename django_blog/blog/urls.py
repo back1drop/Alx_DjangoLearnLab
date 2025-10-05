cc5f74ef81abdd1d1138ee807fa6a1cc42b730c5
Micah <166214972+back1drop@users.noreply.github.com>
2025-10-05 09:20:08 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -21,4 +21,8 @@ urlpatterns = [
     path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='add-comment'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+
+      # --- Tag & Search URLs ---
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts-by-tag'),
+    path('search/', views.search_posts, name='search-posts'),
 ]
